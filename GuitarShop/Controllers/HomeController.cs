1bcaa4dac1db374b4789e4d26581af1e195a30c0
Ennis7 <Sarah.Ennis231@gmail.com>
2024-10-20 20:18:15 -0400

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        //Step 14.)
+        [Route("[action]")]
+        public IActionResult ContactUs()
+        {
+            //Step 19.)
+            var contact = new Dictionary<string, string> {                 { "Phone", "555-123-4567" },                 { "Email", "info@myguitarshop.com" }            };
+            return View(contact);
+        }
+    }
+}
